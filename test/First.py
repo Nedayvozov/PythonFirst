b3f49a21a3b0529cf9f20260960095119893dafc
Pavel <Avant_npg@mail.ru>
2018-02-20 17:38:46 +0200

    add simple function

--- test/First.py
+++ test/First.py
@@ -1,1 +1,13 @@
 # test# test# test# test
+
+
+
+
+def greet(name):
+	"""This function greets to
+	the person passed in as
+	parameter"""
+	print("Hello, " + name + ". Good morning!")
+
+
+greet('Paul')
